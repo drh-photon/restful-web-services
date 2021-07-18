0248b4c5bc0c49751d3e2d6b3493e2ddc77b4434
donald_h <donald_h@192.168.39.135>
2021-07-18 05:20:33 -0700

    added param data

--- src/main/java/com/restful/rest/webservices/restfulwebservices/RestfulController.java
+++ src/main/java/com/restful/rest/webservices/restfulwebservices/RestfulController.java
@@ -1,6 +1,7 @@
 package com.restful.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,9 @@ public class RestfulController {
 	@GetMapping(path = "/api")
 	public RestfulBean restfulBean() {
 		return new RestfulBean("restful");
+	}
+	@GetMapping(path = "/api/path/{data}")
+	public RestfulBean restfulBeanPV(@PathVariable String data) {
+		return new RestfulBean(String.format("Data: %s", data));
 	}
 }
